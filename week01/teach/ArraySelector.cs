cb11e6b90c29fa312b60cbfdfc61a28ec64f35eb
o-j2315 <jhernandezap23@gmail.com>
2025-09-03 21:27:02 -0600

    Implement ListSelector logic and update Program.cs to run DivisorsSolution and ArraySelectorSolution

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,26 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
+        List<int> Results = new();
+        int n1 = 0;
+        int n2 = 0;
+        for (int i = 0; i < select.Length; i++)
+        {
+            if (select[i] == 1)
+            {
+                Results.Add(list1[n1]);
+                n1++;
+            }
+            else if (select[i] == 2)
+            {
+                Results.Add(list2[n2]);
+                n2++;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Select value must be 1 or 2");
+            }
+        }
         return [];
     }
 }
