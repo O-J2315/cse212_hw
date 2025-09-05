2fb64380cae3a640481b9e51578f1a0cca83a414
o-j2315 <jhernandezap23@gmail.com>
2025-09-05 16:53:34 -0600

    Implement MultiplesOf function and enhance RotateListRight logic for array manipulation

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -8,12 +8,23 @@ public static class Arrays
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
     public static double[] MultiplesOf(double number, int length)
     {
-        // TODO Problem 1 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        //Plan:
+        //First we create an array with the length given to store the multiples of the number given
+        // Function will iterate through the lenght of the number given and multiply the number 
+        // by the index in which I is currently at.
+        //The multiple is stored in a variable 
+        //The multiple is then stored in the array at the index of I
+        //Return the resulting array
 
-        return []; // replace this return statement with your own
+
+        double[] multiplesArray = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            double multiple = number * (i + 1);
+            multiplesArray[i] = multiple;
+        }
+        return multiplesArray; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +40,29 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        //Plan:
+        //First we create a new list 
+        //We iterate through the list of data that is going to be rotated selecting the last 'amount' of numbers 
+        //Using a foreach loop with the range method
+        //we assign each number rotated to the new list
+        //With the other part of the list of numbers we iterate and add them to the new list
+        //Finally we clear the original list and add the new rotated list to it
+
+        List<int> rotatedList = new List<int>(data.Count);
+
+        foreach (int number in data.GetRange(data.Count - amount, amount))
+        {
+            rotatedList.Add(number);
+        }
+
+        foreach (int number in data.GetRange(0, data.Count - amount))
+        {
+            rotatedList.Add(number);
+
+
+        }
+        data.Clear();
+        data.AddRange(rotatedList);
     }
 }
